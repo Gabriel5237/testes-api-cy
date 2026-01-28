f87bba48403bc22f09ef830b78c06a6e67ea3685
Gabriel Henrique <gabriel55237@gmail.com>
2026-01-28 20:16:08 -0300

    testes-API-cy

---
+++ testes-api.js
@@ -0,0 +1,144 @@
+describe('Testes da Funcionalidade Usuários', () => {
+
+  it('Deve validar contrato de usuários', () => {
+
+    cy.request('/users').then((response) => {
+
+      expect(response.status).to.eq(200)
+
+      expect(response.body[0]).to.have.property('id')
+      expect(response.body[0]).to.have.property('name')
+      expect(response.body[0]).to.have.property('username')
+      expect(response.body[0]).to.have.property('email')
+
+      expect(response.body[0].address).to.have.property('street')
+      expect(response.body[0].address).to.have.property('city')
+
+      expect(response.body[0]).to.have.property('phone')
+      expect(response.body[0]).to.have.property('website')
+
+    })
+
+  })
+
+})
+
+describe('Testes da Funcionalidade Usuários', () => {
+
+  it('Deve listar usuários cadastrados', () => {
+    cy.request('/users').then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body).to.be.an('array')
+      expect(response.body.length).to.be.greaterThan(0)
+    })
+  })
+
+  it('Deve validar contrato de usuários', () => {
+    cy.request('/users').then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body[0]).to.have.property('id')
+      expect(response.body[0]).to.have.property('name')
+      expect(response.body[0]).to.have.property('email')
+    })
+  })
+
+})
+
+describe('Testes da Funcionalidade Usuários', () => {
+
+  it('Deve listar usuários cadastrados', () => {
+    cy.request('/users').then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body.length).to.be.greaterThan(0)
+    })
+  })
+
+  it('Deve validar contrato de usuários', () => {
+    cy.request('/users').then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body[0]).to.have.property('id')
+      expect(response.body[0]).to.have.property('name')
+      expect(response.body[0]).to.have.property('email')
+    })
+  })
+
+  it('Deve cadastrar um usuário com sucesso', () => {
+
+    const usuario = {
+      name: "Gabriel Henrique",
+      username: "gabriel.qa",
+      email: "gabriel@teste.com"
+    }
+
+    cy.request({
+      method: 'POST',
+      url: '/users',
+      body: usuario
+    }).then((response) => {
+      expect(response.status).to.eq(201)
+      expect(response.body).to.have.property('id')
+    })
+
+  })
+
+})
+
+describe('Testes da Funcionalidade Usuários', () => {
+
+  it('Deve validar um usuário com email inválido', () => {
+
+    const usuario = {
+      name: "Gabriel Henrique",
+      username: "gabriel.qa",
+      email: "gabriel.com.br" // email inválido
+    }
+
+    // regex para validar formato de email
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    // validação
+    expect(usuario.email).to.not.match(regexEmail)
+
+  })
+
+})
+
+describe('Testes da Funcionalidade Usuários', () => {
+
+  it('Deve editar um usuário previamente cadastrado', () => {
+
+    const usuarioEditado = {
+      name: "Gabriel Henrique Editado",
+      email: "gabriel.editado@teste.com"
+    }
+
+    cy.request({
+      method: 'PUT',
+      url: '/users/1',
+      body: usuarioEditado
+    }).then((response) => {
+      expect(response.status).to.eq(200)
+      expect(response.body.name).to.eq(usuarioEditado.name)
+      expect(response.body.email).to.eq(usuarioEditado.email)
+
+    })
+
+  })
+
+})
+
+describe('Testes da Funcionalidade Usuários', () => {
+
+  it('Deve deletar um usuário previamente cadastrado', () => {
+
+    cy.request({
+      method: 'DELETE',
+      url: '/users/1'
+    }).then((response) => {
+      expect(response.status).to.eq(200)
+
+    })
+
+  })
+
+})
